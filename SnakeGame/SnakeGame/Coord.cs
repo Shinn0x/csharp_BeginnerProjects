95f1dd046c5c17341822aed9008f718aa735fff6
Shinn0x <148429347+Shinn0x@users.noreply.github.com>
2025-04-26 01:10:02 +0800

    Snake Game Roughly Finish

---
+++ SnakeGame/SnakeGame/Coord.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeGame
+{
+    internal class Coord
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Coord(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if((obj == null) || !GetType().Equals(obj.GetType())) return false;
+
+            Coord other = (Coord)obj;
+            return X == other.X && Y == other.Y;
+        }
+
+        public void ApplyMovementDirection(Direction direction)
+        {
+            switch (direction)
+            {
+                case Direction.Left:
+                    X--;
+                    break;
+                case Direction.Right:
+                    X++;
+                    break;
+                case Direction.Up:
+                    Y--;
+                    break;
+                case Direction.Down:
+                    Y++;
+                    break;
+
+
+            }
+        }
+    }
+}
